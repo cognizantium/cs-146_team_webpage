b7489903eb5afd48b7fe518c997c5d4cc33f031d
sergescardigno <32397861+sergescardigno@users.noreply.github.com>
2017-12-03 23:59:41 -0500

    Added stuff for the slider, doesn't work

--- js/javaandpotatoes.js
+++ js/javaandpotatoes.js
@@ -1,3 +1,13 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+alert(slider.value);
+
 function handleFormSubmit() {
 	if(window.event){
 		window.event.preventDefault();
