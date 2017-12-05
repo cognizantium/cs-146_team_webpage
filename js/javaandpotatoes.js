c98df7bedfc3a990dd5b8c039d27e7f49cf7e7ff
cognizantium <32963776+cognizantium@users.noreply.github.com>
2017-12-04 20:20:27 -0500

    fixed sterrrrrfffffffffffffffffffffffff

--- js/javaandpotatoes.js
+++ js/javaandpotatoes.js
@@ -26,26 +26,6 @@ function addImageEvents() {
     }
 }
 
-window.addEventListener("load", function() {
-    addImageEvents();
-});
-
-function addImageEvents() {
-    var images = document.getElementsByTagName("img");
-    console.log(images);
-
-    for (var i = 1; i < images.length; ++i) {
-        images[i].onmouseover = function(e) {
-            console.log("stuff");
-        }
-    }
-    for (var i = 1; i < images.length; ++i) {
-        images[i].onmouseout = function(e) {
-            console.log("stuff");
-        }
-    }
-}
-
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
